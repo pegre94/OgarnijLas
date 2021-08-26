49ebc92ca584106281f87d34db4aecaf52f1dbde
Przemyslaw Grenda <przemekgrenda@gmail.com>
2021-08-26 11:00:52 +0200

    Center region to Poland.

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Platform, Text, View, StyleSheet } from "react-native";
+import { Platform, Text, View, StyleSheet, Dimensions } from "react-native";
 import * as Location from "expo-location";
 import MapView from "react-native-maps";
 
@@ -28,8 +28,31 @@ export default function App() {
   }
 
   return (
-    <View>
+    <View style={styles.container}>
       <Text>{text}</Text>
+      <MapView
+        s
+        initialRegion={{
+          latitude: 52.1127,
+          longitude: 19.2119,
+          latitudeDelta: 10,
+          longitudeDelta: 10,
+        }}
+        style={styles.map}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+});
