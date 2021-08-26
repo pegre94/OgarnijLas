83e6a676a71f60a4d549b6a591ff7f490e7a7ee4
Przemyslaw Grenda <przemekgrenda@gmail.com>
2021-08-26 15:03:45 +0200

    Modal is working.

--- App.js
+++ App.js
@@ -1,9 +1,27 @@
-import React, { useState, useEffect } from "react";
-import { Platform, Text, View, StyleSheet, Dimensions } from "react-native";
+import React, {
+  useState,
+  useEffect,
+  useCallback,
+  useMemo,
+  useRef,
+} from "react";
+import {
+  Platform,
+  Text,
+  View,
+  StyleSheet,
+  Dimensions,
+  Button,
+} from "react-native";
 import { FAB } from "react-native-paper";
-
 import * as Location from "expo-location";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import {
+  BottomSheetModal,
+  BottomSheetModalProvider,
+} from "@gorhom/bottom-sheet";
 
 export default function App() {
   const [region, setRegion] = useState({
@@ -12,10 +30,81 @@ export default function App() {
     latitudeDelta: 10,
     longitudeDelta: 10,
   });
+
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
+  const [modal, setModal] = useState(false);
+  // BOTTOM SHEET
+  // ref
+  const bottomSheetModalRef = useRef(BottomSheetModal);
 
-  const getLocation = async () => {
+  // variables
+  const snapPoints = useMemo(() => ["25%", "50%"], []);
+
+  // callbacks
+  const handlePresentModalPress = useCallback(() => {
+    bottomSheetModalRef.current?.present();
+  }, []);
+
+  const handleSheetChanges = useCallback(
+    (index) => {
+      if (index == 1) {
+        setModal(true);
+      } else if (index == -1) {
+        setModal(false);
+      }
+    },
+    [modal]
+  );
+  // BOTTOM SHEET END
+
+  const trashList = [
+    {
+      latlng: {
+        latitude: 51.9194,
+        longitude: 19.1451,
+      },
+      title: "test",
+      description: "test description",
+    },
+    {
+      latlng: {
+        latitude: 51.4194,
+        longitude: 19.2451,
+      },
+      title: "test",
+      description: "test description",
+    },
+    {
+      latlng: {
+        latitude: 51.7194,
+        longitude: 19.0451,
+      },
+      title: "test",
+      description: "test description",
+    },
+  ];
+
+  const TrashMarkers = () => {
+    return trashList.map((marker, index) => {
+      return (
+        <Marker
+          key={index}
+          coordinate={marker.latlng}
+          title={marker.title}
+          description={marker.description}
+          onPress={(e) => console.log(e.nativeEvent)}
+        >
+          <MaterialCommunityIcons
+            name="trash-can-outline"
+            size={32}
+            color="black"
+          />
+        </Marker>
+      );
+    });
+  };
+  const getLocation = async (d1) => {
     let { status } = await Location.requestForegroundPermissionsAsync();
     if (status !== "granted") {
       setErrorMsg("Permission to access location was denied");
@@ -32,18 +121,11 @@ export default function App() {
       setRegion({
         latitude: location.coords.latitude,
         longitude: location.coords.longitude,
-        latitudeDelta: 0.001,
-        longitudeDelta: 0.001,
+        latitudeDelta: d1,
+        longitudeDelta: d1,
       });
     }
   };
-
-  let text = "Waiting..";
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
-  }
 
   return (
     <View style={styles.container}>
@@ -51,7 +133,29 @@ export default function App() {
         region={region}
         onRegionChangeComplete={(region) => setRegion(region)}
         style={styles.map}
-      />
+        onMapReady={() => getLocation(0.005)}
+      >
+        <TrashMarkers />
+      </MapView>
+      <BottomSheetModalProvider>
+        <View style={styles.drawerContainer}>
+          <Button
+            onPress={handlePresentModalPress}
+            title="Present Modal"
+            color="black"
+          />
+          <BottomSheetModal
+            ref={bottomSheetModalRef}
+            index={1}
+            snapPoints={snapPoints}
+            onChange={handleSheetChanges}
+          >
+            <View style={styles.drawerContentContainer}>
+              <Text>Awesome</Text>
+            </View>
+          </BottomSheetModal>
+        </View>
+      </BottomSheetModalProvider>
       <View style={styles.fabs}>
         <FAB
           icon="crosshairs-gps"
@@ -59,7 +163,7 @@ export default function App() {
             flex: 1,
             marginBottom: 14,
           }}
-          onPress={() => getLocation()}
+          onPress={() => getLocation(0.002)}
         />
         <FAB
           icon="plus"
@@ -81,6 +185,18 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "100%",
   },
+  drawerContainer: {
+    position: "absolute",
+    flex: 1,
+    padding: 24,
+    justifyContent: "center",
+    backgroundColor: "grey",
+  },
+  drawerContentContainer: {
+    flex: 1,
+    alignItems: "center",
+  },
+
   fabs: {
     flex: 1,
     position: "absolute",
---
+++ components/markers.js
@@ -0,0 +1,33 @@
+export const TrashMarkers = (trashList) => {
+  return trashList.map((marker, index) => {
+    return (
+      <Marker
+        key={index}
+        coordinate={marker.latlng}
+        title={marker.title}
+        description={marker.description}
+        onPress={(e) => console.log(e.nativeEvent)}
+      >
+        <MaterialCommunityIcons
+          name="trash-can-outline"
+          size={32}
+          color="black"
+        />
+        {/* <MapView.Callout tooltip> */}
+        {/*   <TouchableHighlight */}
+        {/*     onPress={() => this.markerClick()} */}
+        {/*     underlayColor="#dddddd" */}
+        {/*   > */}
+        {/*     <View> */}
+        {/*       <Text> */}
+        {/*         {marker.title} */}
+        {/*         {"\n"} */}
+        {/*         {marker.description} */}
+        {/*       </Text> */}
+        {/*     </View> */}
+        {/*   </TouchableHighlight> */}
+        {/* </MapView.Callout> */}
+      </Marker>
+    );
+  });
+};
